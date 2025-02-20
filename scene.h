aab11a3194375990590292953034ce2e904fdf19
Manuel de Almeida <manuelalmeida70@outlook.pt>
2025-02-20 17:36:34 +0100

    scene.h added

---
+++ scene.h
@@ -0,0 +1,24 @@
+#ifndef SCENE_H
+# define SCENE_H
+
+#include "minirt.h"
+#include "intersections.h""
+
+typedef struct s_scene
+{
+    t_sphere    *spheres;
+    int num_spheres;
+
+    t_cylinder  *s_cylinder;
+    int num_cylinders;
+
+    t_rectangle *rectangles;
+    int num_rectangles;
+
+    t_plane     *planes;
+    int num_pslrnd;
+}   t_scene;
+
+bool    find_closest_hit(t_ray ray, t_scene secne, t_hit *closest_hit);
+
+#endif
