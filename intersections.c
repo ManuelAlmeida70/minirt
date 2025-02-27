2f025bd18577c8add7a5e97c00146cce159ba4de
Manuel de Almeida <manuelalmeida70@outlook.pt>
2025-02-27 08:15:22 +0100

    functions intersect_cylinder and intersect_rectangle done

--- intersections.c
+++ intersections.c
@@ -95,3 +95,55 @@ bool	intersect_plane(t_ray ray, t_plane plane, t_hit *hit)
 	hit->color = plane.color;
 	return true;
 }
+
+//Intersecao com cilindro(Equacao parametrica)
+bool	intersect_cylinder(t_ray ray, t_cylinder cylinder, t_hit *hit)
+{
+	t_vector oc = vector_subtract(ray.origin, cylinder.center);
+	t_vector d = cylinder.direction;
+	vector_normalize(&d);
+
+	t_vector v = vector_subtract(ray.direction, vector_scale(d, vector_dot(ray.direction, d)));
+	t_vector w = vector_subtract(oc, vector_scale(d, vector_dot(oc, d)));
+
+	double a =  vector_dot(v, v);
+	double b = 2.0 * vector_dot(v, w);
+	double c = vector_dot(w, w) - (cylinder.radius * cylinder.radius);
+	double discriminant = b * b - 4 * a * c;
+
+	if (discriminant < 0)
+		return false;
+	
+		double t1 = (-b - sqrt(discriminant)) / (2.0 * a);
+		double t2 = (-b + sqrt(discriminant)) / (2.0 * a);
+		double t = (t1 > 0) ? t1 : t2;
+
+		if (t < 0)
+			return false;
+
+		hit->t = t;
+		hit->point = ray_at(ray, t);
+		hit->normal = vector_subtract(hit->point, cylinder.center);
+		vector_normalize(&hit->normal);
+		hit->color = cylinder.color;
+		return true;
+}
+
+bool	intersect_rectangle(t_ray ray, t_rectangle rectangle, t_hit *hit)
+{
+	double denom = vector_dot(rectangle.normal, ray.direction);
+
+	if (fabs(denom) < 1e-6)
+		return false;
+	
+		double t = vector_dot(vector_subtract(rectangle.point, ray.origin));
+
+		if (t < 0)
+			return false;
+		
+			hit->t = t;
+			hit->point = ray_at(ray, t);
+			hit->normal = rectangle.normal;
+			hit->color = rectangle.color;
+			return true;
+}
